1e64e7bfb904f4d1824f2a3ab77e34980802b4a5
AlizadaG <elizadegozel@gmail.com>
2024-05-27 13:22:46 +0400

    Array test

--- 27.05.2024/Arrays/Arrays/Program.cs
+++ 27.05.2024/Arrays/Arrays/Program.cs
@@ -1,13 +1,17 @@
-﻿namespace Arrays
+﻿using System;
+
+namespace Arrays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            PracticeArrayCustom();
             PracticeArray();
+            AverageArray();
         }
 
-        public static void PracticeArray(){
+        public static void PracticeArrayCustom(){
             int[] numbers = { 0, 50, -11, 23, 18 };
             int min = 0, index = 0;
             for (int i = 0; i < numbers.Length; i++) { 
@@ -18,7 +22,38 @@
                 }
             }
             Console.WriteLine("Minimum eded {0} - dir ve indeksi {1}- dir", min, index);
-        }   
+        }
+
+        public static void PracticeArray()
+        {
+            int[] numbers = { 0, 50, -11, 23, 18 };
+            Array.Sort(numbers);
+            Console.WriteLine("Minimum eded {0} - dir", numbers[0]);
+           
+            int min = 0, index = 0;
+            Array.ForEach(numbers, n =>
+            {
+                if (n < min)
+                {
+                    min = n;
+                    index = Array.IndexOf(numbers, n);
+                }
+            });
+            Console.WriteLine("Minimum eded {0} - dir ve indeksi {1}- dir", min, index);
+        }
+
+        public static void AverageArray()
+        {
+            int[] numbers = { 0, 50, -11, 23, 18 };
+            int sum = 0;
+            float avg = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            avg = sum / numbers.Length;
+            Console.WriteLine("Arrayin ortalamasi {0} - dir", avg);
+        }
 
     }
 }
--- 27.05.2024/Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ 27.05.2024/Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06a11a79c48730c5f15a218533ac8e3d3728d8e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f133139bdef498ad027b5ae1e875bc3a4635dc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
