8f133139bdef498ad027b5ae1e875bc3a4635dc5
AlizadaG <elizadegozel@gmail.com>
2024-05-27 13:06:38 +0400

    Arrayda min ededin ozu ve indexin tapan funk

---
+++ 24.05.2024/Arrays/Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Zehmet olmasa simvol daxil edin:");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            char mySymb = keyInfo.KeyChar;
+            Console.WriteLine("Sizin daxil etdiyiniz: " + mySymb);
+
+
+            bool isLetter = Char.IsLetter(mySymb);
+            bool isDigit = Char.IsDigit(mySymb);
+            bool isLetterOrDigit = Char.IsLetterOrDigit(mySymb);
+            if (isLetter) { 
+                Console.WriteLine("Sizin daxil etdiyiniz simvol herfdir");
+            }
+            else
+            {
+                Console.WriteLine("Sizin daxil etdiyiniz simvol herf deyil");
+            }
+        }
+    }
+}
---
+++ 27.05.2024/Arrays/Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PracticeArray();
+        }
+
+        public static void PracticeArray(){
+            int[] numbers = { 0, 50, -11, 23, 18 };
+            int min = 0, index = 0;
+            for (int i = 0; i < numbers.Length; i++) { 
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                    index = i;
+                }
+            }
+            Console.WriteLine("Minimum eded {0} - dir ve indeksi {1}- dir", min, index);
+        }   
+
+    }
+}
