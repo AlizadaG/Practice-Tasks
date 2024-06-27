40c53a048f30c4ba17a20457c831eb2045fbcc8a
AlizadaG <elizadegozel@gmail.com>
2024-06-27 12:42:54 +0400

    SQL

---
+++ 27.06.2024/SQLQuery5.sql
@@ -0,0 +1,18 @@
+INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorBirthDate)
+VALUES 
+('John', 'Doe', '1980-05-15'),
+('Jame', 'Smith', '2000-05-15'),
+('Alice', 'JohnSomn', '1980-05-15') 
+
+INSERT INTO Books (BookTitle, BookAuthorId, BookPublicationYear, BookGenre) VALUES
+('BookOne', 1, 2020, 'Fiction'),
+('BookTwo', 2, 2019, 'Non-Fiction'),
+('BookThree', 3, 2021, 'Science Fiction'),
+('BookFour', 1, 2018, 'Mystery'),
+('BookFive', 2, 2022, 'Biography');
+
+
+select * from Authors
+select * from Books
+
+
